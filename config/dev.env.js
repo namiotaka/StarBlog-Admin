01e4d93aa2087407f138fad724f907996c043692
Jade Li (CSI INTERFUSION INC) <v-lijade@microsoft.com>
2024-02-08 14:03:33 +0800

    fixed npm install error

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_URL: '"http://localhost:5038"',
+  BASE_URL: '"http://localhost:9377"',
   EXCEPTION_LESS_API_KEY: '"1suhJwX6x14Ao1UtfK5vhoKfzwun47KvJDvsKCzv"',
   EXCEPTION_LESS_SERVER_URL: '"http://10.116.163.185:5000"',
 })
